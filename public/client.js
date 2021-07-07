977af489d8c9eeeb6e9855cf97affdcb5f2082c4
Ramesh Kumar <developer.rameshraj@gmail.com>
2021-07-07 16:24:50 +0530

    webRTC video chat

---
+++ public/client.js
@@ -0,0 +1,254 @@
+$('.room').click(function () {
+  if (($('#uname').val() != '') && ($('#uroom').val() != '')) {
+    $('#wrapper').show();
+    $('#room_wrapper').hide('slow');
+    userName = $('#uname').val();
+    room = $('#uroom').val();
+
+
+
+
+
+
+
+
+    //let userName = prompt("What's your name");
+    //let room = prompt("Room name");
+    $('#userName').text(userName);
+
+    //here we connect to the socket.io server 
+    var socket = io();
+
+    let ID = "";
+    var divConsultingRoom = document.getElementById('consultingRoom');
+    var localVideo = document.getElementById('localVideo');
+    var remoteVideo = document.getElementById('remoteVideo');
+    
+    //these are the global variables
+    var localStream;
+    var remoteStream;
+    var rtcPeerConnection;
+    //these are the STUN servers
+    var iceServers = {
+      'iceServers': [
+        { 'url': 'stun:stun.services.mozilla.com' },
+        { 'url': 'stun:stun.i.google.com:19302' },
+      ]
+    }
+    var streamConstraints = { audio: true, video: true };
+    var isCaller;
+
+    if (room) {
+      //send event that user has joined room to server
+      socket.emit('create or join', { username: userName, roomName: room });
+      divConsultingRoom.style = 'display:block';
+    }
+
+    //receive data from server.
+    socket.on('send data', (data) => {
+      ID = data.id; //ID will be used later
+      console.log(" my ID:" + ID);
+    });
+
+    $("#usermsg").focus();
+    //when form is submitted, capture the input value and then send it to server
+    document.getElementsByTagName("form")[0].addEventListener("submit", function (event) {
+      event.preventDefault();
+      socket.emit("chat message", {
+        value: document.getElementById("usermsg").value,
+        user: userName,
+      });
+      document.getElementById("usermsg").value = "";
+    });
+
+
+    // Picks up the data emitted by the server and displays the message to the webpage
+    socket.on("chat message", (data) => {
+      console.log(data.data.user + ": " + data.id);
+      displayMessage(data);
+    });
+
+
+    function displayMessage(data) {
+      let authorClass = "";
+      let divClass = ""
+      //verify that the user ID and the message sent ID is similar 
+      if (data.id === ID) {
+        console.log("This person has sent a message")
+        authorClass = "me";
+        divClass = "myDiv";
+      } else {
+        authorClass = "you";
+        divClass = "yourDiv";
+      }
+      const div = document.createElement("div");
+      div.className = divClass;
+      const li = document.createElement("li");
+      const p = document.createElement("p");
+      div.innerHTML =
+        '<p class="' +
+        authorClass +
+        '">' +
+        data.data.user + ', ' + moment().format("hh:mm A") +
+
+        "</p>" +
+        '<p class="message"> ' +
+        data.data.value +
+        "</p>";
+      div.appendChild(p);
+      li.appendChild(div);
+
+      document.getElementById("messages").appendChild(li);
+
+      window.scrollTo(0, document.body.scrollHeight); //scroll to the bottom
+    }
+
+
+
+
+    //when server emits created
+    socket.on('created', function (room) {
+      //caller gets media devices with defined constraints
+      navigator.mediaDevices.getUserMedia(streamConstraints).then(function (stream) {
+        localStream = stream;
+        localVideo.srcObject = stream;
+        isCaller = true;
+      }).catch(function (err) {
+        console.log('An error occured when accessing media devices');
+      })
+    });
+
+
+    //when server emits joined
+    socket.on('joined', function (room) {
+      //callee gets user media devices
+      navigator.mediaDevices.getUserMedia(streamConstraints).then(function (stream) {
+        localStream = stream;
+        localVideo.srcObject = stream;
+        socket.emit('ready', room);
+      }).catch(function (err) {
+        console.log('An error ocurred when accessing media devices');
+      });
+    });
+
+    //when server emits ready
+    socket.on('ready', function () {
+      if (isCaller) {
+        //creates an RTCPeerConnection object
+        rtcPeerConnection = new RTCPeerConnection(iceServers);
+
+        //add event listners to the newly created object
+        rtcPeerConnection.onicecandidate = onIceCandidate;
+        rtcPeerConnection.onaddstream = onAddStream;
+
+        //add the current local stream to the object
+        rtcPeerConnection.addStream(localStream);
+
+        //prepares an offer
+        rtcPeerConnection.createOffer(setLocalAndOffer, function (e) {
+          console.log(e);
+        });
+      }
+    });
+
+
+    //when servers emits offer
+    socket.on('offer', function (event) {
+      if (!isCaller) {
+        //creates an RTCPeerConnection object
+        rtcPeerConnection = new RTCPeerConnection(iceServers);
+
+        //adds events listners to newly created
+        rtcPeerConnection.onicecandidate = onIceCandidate;
+        rtcPeerConnection.onaddstream = onAddStream;
+
+        //adds the current local stream to the object
+        rtcPeerConnection.addStream(localStream);
+
+        //stores the offer as remote description
+        rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
+
+        //Preparees an Answer
+        rtcPeerConnection.createAnswer(setLocalAndAnswer, function (e) {
+          console.log(e);
+        });
+
+      }
+    });
+
+
+    //when server emits answer
+    socket.on('answer', function (event) {
+      //stores it as remot description
+      rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
+    });
+
+    //when server emits candidate
+    socket.on('candidate', function (event) {
+      //creates a candidate object
+      var candidate = new RTCIceCandidate({
+        sdpMLineIndex: event.label,
+        candidate: event.candidate
+      });
+      //stores candidate
+      rtcPeerConnection.addIceCandidate(candidate);
+    });
+
+
+    //when a user receive the user's video and audio stream 
+    function onAddStream(event) {
+      remoteVideo.srcObject = event.stream;
+      remoteStream = event.stream;
+    }
+
+    //these are the functions referenced before as listeners for the peer connection
+    //send a candidate meesage to server
+    function onIceCandidate(event) {
+      if (event.candidate) {
+        console.log('sending ice candidate');
+        socket.emit('candidate', {
+          type: 'candidate',
+          label: event.candidate.sdpMLineIndex,
+          id: event.candidate.sdpMid,
+          candidate: event.candidate.candidate,
+          room: room
+        });
+      }
+    }
+
+
+    //stores offer and seeds message to server
+    function setLocalAndOffer(sessionDescription) {
+      rtcPeerConnection.setLocalDescription(sessionDescription);
+      socket.emit('offer', {
+        type: 'offer',
+        sdp: sessionDescription,
+        room: room
+      });
+    }
+
+    //stores answer and send message to server
+    function setLocalAndAnswer(sessionDescription) {
+      rtcPeerConnection.setLocalDescription(sessionDescription);
+      socket.emit('answer', {
+        type: 'answer',
+        sdp: sessionDescription,
+        room: room
+      });
+    }
+
+
+    
+    $('#mute').click(function(){
+      localStream.getAudioTracks().forEach(track => track.enabled = !track.enabled);
+      $(this).val( ($(this).val() == 'Mute') ? 'Unmute' : 'Mute' );
+    });
+
+    $('#camera').click(function(){
+      localStream.getVideoTracks().forEach(track => track.enabled = !track.enabled);
+      $(this).val( ($(this).val() == 'Camera Off') ? 'Camera On' : 'Camera Off' );
+    });
+
+
+  }
+});
--- server.js
+++ server.js
@@ -6,28 +6,43 @@ const { joinUser, removeUser, findUser } = require('./users');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function (req, res) {
+app.get("/message/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 let thisRoom = "";
-io.on("connection", function (socket) {
-  console.log("connected");
-  socket.on("join room", (data) => {
-    console.log('in room');
+
+
+io.on('connection', function (socket) {
+  console.log(socket.id , 'a user connected');
+
+  //when client emits create or join
+  socket.on('create or join', function (data) {
+
     let Newuser = joinUser(socket.id, data.username, data.roomName)
     //io.to(Newuser.roomname).emit('send data' , {username : Newuser.username,roomname : Newuser.roomname, id : socket.id})
     // io.to(socket.id).emit('send data' , {id : socket.id ,username:Newuser.username, roomname : Newuser.roomname });
-    socket.emit('send data', {
-      id: socket.id,
-      username: Newuser.username,
-      roomname: Newuser.roomname
-    }
-    );
-
+    socket.emit('send data', { id: socket.id, username: Newuser.username, roomname: Newuser.roomname });
     thisRoom = Newuser.roomname;
-    console.log(Newuser);
     socket.join(Newuser.roomname);
+    console.log('create or join to room', Newuser.roomname);
+
+
+    //count number of users on room
+    var myRoom = io.sockets.adapter.rooms[Newuser.roomname] || { length: 0 };
+    var numClients = myRoom.length;
+    console.log(Newuser.roomname, ' has ', numClients, ' clients');
+    if (numClients == 1) {
+      socket.join(Newuser.roomname);
+      socket.emit('created', Newuser.roomname);
+      //socket.to(roomId).broadcast.emit('user-connected', userId);
+    } else if (numClients == 2) {
+      socket.join(Newuser.roomname);
+      socket.emit('joined', Newuser.roomname);
+    } else {
+      socket.emit('full', Newuser.roomname);
+    }
+
   });
 
 
@@ -35,8 +50,6 @@ io.on("connection", function (socket) {
   socket.on("chat message", (data) => {
     io.to(thisRoom).emit("chat message", { data: data, id: socket.id });
   });
-
-
 
   socket.on("disconnect", () => {
     const user = removeUser(socket.id);
@@ -48,11 +61,31 @@ io.on("connection", function (socket) {
 
   });
 
+
+  //relay only handlers
+  socket.on('ready', function (room) {
+    socket.broadcast.to(room).emit('ready');
+  })
+
+  socket.on('candidate', function (event) {
+    socket.broadcast.to(event.room).emit('candidate', event);
+  });
+
+  socket.on('offer', function (event) {
+    socket.broadcast.to(event.room).emit('offer', event.sdp);
+  });
+
+  socket.on('answer', function (event) {
+    socket.broadcast.to(event.room).emit('answer', event.sdp);
+  });
+
 });
 
 
-const port = process.env.PORT || 3000; 
 
+
+
+const port = process.env.PORT || 3000;
 http.listen(port, function () {
   console.log("Server listening on port 3000.");
 });
